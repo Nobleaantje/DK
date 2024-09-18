1bd688754113ab10a42f30e7ad674abf4be02f4e
Jerrel Unkel <jerrel.unkel@nobleo.nl>
2024-09-18 19:23:14 +0200

    First iteration

--- bot.py
+++ bot.py
@@ -5,11 +5,13 @@ from pygame import Vector2
 from ...bot import Bot
 from ...linear_math import Transform
 
+import math
 
-class SimpleBot(Bot):
+
+class DK(Bot):
     @property
     def name(self):
-        return "SimpleBot"
+        return "DK"
 
     @property
     def contributor(self):
@@ -19,15 +21,29 @@ class SimpleBot(Bot):
         target = self.track.lines[next_waypoint]
         # calculate the target in the frame of the robot
         target = position.inverse() * target
+
         # calculate the angle to the target
         angle = target.as_polar()[1]
 
         # calculate the throttle
-        target_velocity = 50
-        if velocity.length() < target_velocity:
+        target_waypoint_velocity = 150
+        acc = -100
+
+        distToTarget = math.sqrt(target[0]**2 + target[1]**2)
+        absoluteVelocity = math.sqrt(velocity[0]**2 + velocity[1]**2)
+
+        bTerm = 2*acc*distToTarget + absoluteVelocity**2
+        print(absoluteVelocity)
+
+        if bTerm < 0:
             throttle = 1
         else:
-            throttle = -1
+            t = (math.sqrt(2*acc*distToTarget + absoluteVelocity**2) - absoluteVelocity)/acc
+            vTarget = acc * t + absoluteVelocity
+            if vTarget < target_waypoint_velocity:
+                throttle = 1
+            else:
+                throttle = -1
 
         # calculate the steering
         if angle > 0:
